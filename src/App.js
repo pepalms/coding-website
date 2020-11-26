d9f9386421569cbaf6e32fae21884f83ca28100b
Horationel <73690082+Horationel@users.noreply.github.com>
2020-11-26 10:50:34 +0000

    Mass changes - Harry

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import "./App.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Container from "@material-ui/core/Container";
+import Table from "./Table";
+import Box from "@material-ui/core/Box";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function App() {
-  return <div className="App"></div>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm">
+      <div className={classes.root}>
+        <AppBar color="secondary" position="fixed">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon color="inherit" />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              First attempt using Material-UI
+            </Typography>
+            <Button variant="contained" color="secondary">
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Box mt={15}>
+        <Table />
+      </Box>
+    </Container>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <div>
-      <span>Does it send this way?</span>
-      <span>John's div</span>
-      <span>Harry's div</span>
-    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
