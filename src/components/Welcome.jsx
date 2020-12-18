abef6b1d24221264ab8885ca834d7d8ec9601e82
pepalms <51913936+pepalms@users.noreply.github.com>
2020-12-18 16:44:27 +0000

    Added pictures to welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import { Grid, Paper, Typography } from "@material-ui/core";
+import technical from "../images/techSkills.jpg";
+import softSkills from "../images/agileWork.jpg";
 
 const useStyles = makeStyles((theme) => ({
   toolBar: {
@@ -28,14 +30,48 @@ const useStyles = makeStyles((theme) => ({
   },
   welcomeContainer: {
     paddingTop: "2rem",
+    margin: "auto",
   },
-  hide: {
-    display: "none",
+  hideWhenSmall: {
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
   },
+  hideWhenReallySmall: {
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
+  },
+  subBanner: {
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    height: "40vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    paddingTop: "2rem",
+    "&:hover": {
+      cursor: "pointer",
+      boxShadow: `0 0.4rem 1.4rem 0 ${theme.palette.primary.main}`,
+      transform: "translateY(-0.1rem)",
+      transition: "transform 150ms",
+    },
+  },
+  skillsInformation: {
+    backgroundColor: "rgba(0,0,0, 0.5)",
+    width: "100%",
+    padding: "5%",
+  },
+  technical: { backgroundImage: `url(${technical})` },
+  soft: { backgroundImage: `url(${softSkills})` },
 }));
 
 export default function Welcome(props) {
   const classes = useStyles();
+  //  needed to make the page move with the drawer might be worth turning off for mobile
 
   return (
     <div
@@ -43,16 +79,54 @@ export default function Welcome(props) {
         [classes.contentShift]: props.isDrawerOpen,
       })}
     >
+      {/* shifts the page down under the toolbar */}
+
       <div className={classes.toolBar} />
+
       <Grid
         container
         justify="center"
-        xs={12}
+        xs={11}
         className={classes.welcomeContainer}
+        spacing={6}
       >
-        <Grid item md={5} align="center">
+        {/* main banner */}
+
+        <Grid item md={7} align="center" className={classes.hideWhenSmall}>
           <Paper>
-            <Typography variant="h1"> Learning is good</Typography>
+            <Typography variant="h1">LEARNING IS FUN</Typography>
+            <Typography variant="body1" gutterBottom="true">
+              “Intellectual growth should commence at birth and cease only at
+              death.” - <i> Einstein </i>
+            </Typography>
+            <br />
+            <Typography variant="body1">
+              This is a scrapbook of techniques, it's by no means exhaustive and
+              might actually be wrong in some places buts it's the engineering
+              world as we understand it today (2 months into the scheme).
+            </Typography>
+          </Paper>
+        </Grid>
+
+        {/* sub banners */}
+
+        <Grid item md={6} xs={12} justify="center" align="center">
+          <Paper className={clsx(classes.technical, classes.subBanner)}>
+            <Typography variant="h4"> Technical Skills</Typography>
+            <Typography className={classes.skillsInformation}>
+              These are the skills that are needed to help in the development of
+              products
+            </Typography>
+          </Paper>
+        </Grid>
+
+        <Grid item md={6} xs={12} align="center">
+          <Paper className={clsx(classes.soft, classes.subBanner)}>
+            <Typography variant="h4">Transferable Skills</Typography>
+            <Typography className={`${classes.skillsInformation}`}>
+              These are the skills that are needed in order to work efficiently
+              within the team
+            </Typography>
           </Paper>
         </Grid>
       </Grid>
